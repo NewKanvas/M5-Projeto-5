2bec7d0c83833b47298a5ba650d7eff88adb951b
Jordz <143561413+jordaozz@users.noreply.github.com>
2024-02-29 15:02:19 -0300

    Correção de comandos da tabela SQL

--- Code/tabela.sql
+++ Code/tabela.sql
@@ -1,64 +1,66 @@
+-- Drop do Banco de Dados se Existir
 DROP DATABASE IF EXISTS MOEDAS;
+
+-- Criação do Banco de Dados
 CREATE DATABASE MOEDAS;
 USE MOEDAS;
 
+-- Concessão de Permissões (Tenha cautela ao conceder permissões FILE)
 GRANT FILE ON *.* TO 'root'@'localhost';
+FLUSH PRIVILEGES;
 
--- Cria a Tabela "moeda_tab" - Cadastro de Moedas
-CREATE TABLE moeda_tab (
-	`id_moeda` int(10) PRIMARY KEY auto_increment NOT NULL,
-  `moeda` VARCHAR(200)  NOT NULL,
-  `data_inicio` DATE NOT NULL,
-  `Outras Informações:` VARCHAR(200)
-  );
-  
-  
--- Cria a Tabela "data_tab" - Registro de Dados por data e id_moeda
-CREATE TABLE data_tab (
-	`id_moeda`  int(10) NOT NULL,
-	`data` DATE NOT NULL,
-	`alta` DOUBLE,
-	`baixa` DOUBLE,
-	`abertura` DOUBLE,
-	`fechamento` DOUBLE,
-	`volume` DECIMAL(20, 2),
-	`marketcap` DECIMAL(30, 2)
+-- Criação da Tabela "moeda_cad" - Cadastro de Moedas
+CREATE TABLE moeda_cad (
+    `id_moeda` int(10) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    `moeda` VARCHAR(200) NOT NULL,
+    `data` DATE NOT NULL,
+    `alta` DOUBLE,
+    `baixa` DOUBLE,
+    `abertura` DOUBLE,
+    `fechamento` DOUBLE,
+    `volume` DECIMAL(20, 2),
+    `marketcap` DECIMAL(30, 2)
 );
 
-# Cria a Chave estrangeira para id_moeda em data_tab
-ALTER TABLE data_tab
-	ADD CONSTRAINT fk_moeda
-	FOREIGN KEY (id_moeda) REFERENCES moeda_tab(id_moeda);
-
-
--- IMPORTAÇÕES
--- Função para adicionar moedas de acordo com a tabela csv:
+-- IMPORTAÇÃO DO ARQUIVO CSV "cadastro_moeda.csv" PARA A TABELA moeda_cad
 LOAD DATA INFILE 'Data/cadastro_moeda.csv'
-REPLACE INTO TABLE moeda_tab
+INTO TABLE moeda_cad
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
-LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-SELECT * FROM moeda_tab;
+-- Exibir dados da tabela moeda_cad para verificação
+SELECT * FROM moeda_cad;
 
--- IMPORTAÇÕES
+-- Criação da Tabela "data_tab" - Dados de Criptomoedas
+CREATE TABLE data_tab (
+    `id_moeda` int(10) NOT NULL,
+    `data` DATE NOT NULL,
+    `alta` DOUBLE,
+    `baixa` DOUBLE,
+    `abertura` DOUBLE,
+    `fechamento` DOUBLE,
+    `volume` DECIMAL(20, 2),
+    `marketcap` DECIMAL(30, 2)
+);
+
+-- IMPORTAÇÃO DO ARQUIVO CSV "criptomoedas.csv" PARA A TABELA data_tab
 LOAD DATA INFILE 'Data/criptomoedas.csv'
-REPLACE INTO TABLE data_tab
+INTO TABLE data_tab
 FIELDS TERMINATED BY ','
 ENCLOSED BY ''
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
+-- Exibir dados da tabela data_tab para verificação
 SELECT * FROM data_tab;
 
-
--- JOIN DE TABELAS - CRIANDO UMA NOVA
+-- JOIN DE TABELAS - CRIANDO UMA NOVA TABELA chamada "tabela_completa"
 CREATE TABLE tabela_completa AS
-SELECT moeda_tab.moeda, moeda_tab.data_inicio, data_tab.data, data_tab.alta, data_tab.baixa, data_tab.abertura, data_tab.fechamento, data_tab.volume, data_tab.marketcap # ITENS SELECIONADOS
-FROM moeda_tab #LEFT
-JOIN data_tab  #RIGHT
-ON moeda_tab.id_moeda = data_tab.id_moeda; # CHAVE PRIMARIA E ESTRANGEIRA
+SELECT mc.moeda, mc.data, mc.alta, mc.baixa, mc.abertura, mc.fechamento, mc.volume, mc.marketcap
+FROM moeda_cad mc
+JOIN data_tab dt
+ON mc.id_moeda = dt.id_moeda;
 
-SELECT * FROM tabela_completa
-
+-- Exibir dados da tabela completa para verificação
+SELECT * FROM tabela_completa;
